a32f8e177abca45b3870991478c25e4ab9f1479a
qqtcgp1 <yuquan@Yu-iMac.local>
2016-01-14 20:13:18 +0800

    The transformation is now described by M and C. Superimposing of the result and the video is done via this.

--- extend_3D.m
+++ extend_3D.m
@@ -1,20 +1,26 @@
-FILE = '/Users/yuquan/Downloads/OneDrive/Dropbox/de_terminal copy/Matlab_output.dat';
-scale_factor = 0.027;
+FILE_Matlab_output = '/Users/yuquan/GitHub/de_terminal_copy/Matlab_output2.dat';
+
+scale_factor = 0.027;   % computed from pixel to mm conversion of oct image
+
 num_replicates = 40;
+
 true_data_frame = [5,7,9,11,13,15,17,19,21,25];
 
 
-origin = 0.5*(meshwork_l{1} + meshwork_r{1});
+origin = 0.5*(meshwork_l{1} + meshwork_r{1});   % mid-point of the image
 mesh_opt = struct('mesh_size', [5,5], 'imsiz', size(original{1}), 'dilator_muscle',1, ...
     'dilator_width', 1, 'do_refine',false, 'sphincter_muscle', 1, 'dilator_retreat', 1, 'sphincter_wholewidth', 0);
 
+% mesh object
 [mesh_obj, nx, ny, mesh_right, boundary_node_set] = mesh_iris_3D( top_iris_r{1}, bottom_iris_r_smooth{1}, origin(2), num_replicates,3,2,mesh_opt);
 
 mesh_opt.left_right = 'left';
 mesh_opt.mesh_size = [nx, ny];
 mesh_left_original = mesh_iris_2D( top_iris_l{1},bottom_iris_l_smooth{1}, mesh_opt);
 mesh_left_right = mesh_left_original;
+
 mesh_left_right.node_list(:,1) = - mesh_left_original.node_list(:,1) + 2* origin(2);
+
 num_node_2D = length( mesh_left_original.node_list );
 right_node = mesh_obj.node_list( 1: num_node_2D,:);
 
@@ -40,30 +46,41 @@ end
 
 mesh_obj = flip_orientation(mesh_obj);
 
-% modify the coordinates to fit the cylindrical axis
-% reverse y. exchange y and z. so that the three axis are still in positive
-% orientation. (meaning ex x ey = ez, not -ez).
 
-mesh_obj.node_list(:,2) = - mesh_obj.node_list(:,2);
-mesh_obj.node_list(:,[2 3]) = mesh_obj.node_list(:, [3 2]);
+% one-liner transformation, combined in a single equation:
+zmin = min(-mesh_obj.node_list(:,2)); l = length(mesh_obj.node_list);
+mesh_obj.node_list = scale_factor * ( mesh_obj.node_list * [1,0,0; 0,0,-1; 0,1,0] - [ones(l,1)*origin(2), zeros(l,1), zmin* ones(l,1)]);
 
-% translate in the x-y plane, such that the centre coincide with the
-% origin. only need to subtract first column by origin(2)
+% say the transformation is Y = X * M + C, then M and C is as this:
+M = scale_factor * [1,0,0; 0,0,-1; 0,1,0];
+C = -scale_factor * [ones(l,1)*origin(2), zeros(l,1), zmin* ones(l,1)];
 
-mesh_obj.node_list(:,1) = mesh_obj.node_list(:,1) - origin(2);
 
-% translate in the z direction, so that the minimum z value is 0.
-zmin = min( mesh_obj.node_list(:,3) );
-mesh_obj.node_list(:,3) = mesh_obj.node_list(:,3) - zmin;
-mesh_obj.node_list = mesh_obj.node_list * scale_factor;   %pixel to mm conversion.
-fileID = fopen(FILE, 'w');
+% % Transformations, in steps.
+% 
+% % modify the coordinates to fit the cylindrical axis
+% % reverse y. exchange y and z. so that the three axis are still in positive
+% % orientation. (meaning ex x ey = ez, not -ez).
+% 
+% 
+% mesh_obj.node_list(:,2) = - mesh_obj.node_list(:,2);
+% mesh_obj.node_list(:,[2 3]) = mesh_obj.node_list(:, [3 2]);
+% 
+% % translate in the x-y plane, such that the centre coincide with the
+% % origin. only need to subtract first column by origin(2)
+% 
+% mesh_obj.node_list(:,1) = mesh_obj.node_list(:,1) - origin(2);
+% 
+% % translate in the z direction, so that the minimum z value is 0.
+% zmin = min( mesh_obj.node_list(:,3) );
+% mesh_obj.node_list(:,3) = mesh_obj.node_list(:,3) - zmin;
+% mesh_obj.node_list = mesh_obj.node_list * scale_factor;   %pixel to mm conversion.
+
+fileID = fopen(FILE_Matlab_output, 'w');
 fprintf(fileID, 'Number of boundary nodes: %d\n', length(boundary_node_set));
 fprintf(fileID, 'Number of nodes: %d\n', mesh_obj.num_nodes);
-fprintf(fileID, 'Number of elements: %d\n', mesh_obj.num_elements);
-fprintf(fileID, '<boundary node set>\n');
-fprintf(fileID, '%6d',boundary_node_set);
-fprintf(fileID, '\n</boundary node set>\n');
-fprintf(fileID, '\n');
+fprintf(fileID, 'Number of elements: %d\n<boundary node set>\n', mesh_obj.num_elements);
+fprintf(fileID, '%6d\n</boundary node set>\n\n',boundary_node_set);
 % output to txt file (feb format)
 print_feb(mesh_obj, fileID);
 fclose(fileID);
@@ -83,8 +100,8 @@ for i = 1:length(iris_contour_r)
     iris_contour_l{i}(:,2) = iris_contour_l{i}(:,2) - min(iris_contour_l{i}(:,2));
 end
 
-FILE = '/Users/yuquan/Downloads/OneDrive/Dropbox/de_terminal copy/true_data.dat';
-fileID = fopen(FILE, 'w');
+FILE_Matlab_output = '/Users/yuquan/GitHub/de_terminal_copy/true_data.dat';
+fileID = fopen(FILE_Matlab_output, 'w');
 for i = 1:length(true_data_frame)
     % left iris
     fprintf(fileID, 'step %d left, %d points\n', i, length(iris_contour_l{true_data_frame(i)}));
